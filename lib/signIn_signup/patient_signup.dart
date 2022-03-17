c7691ffcb3ddab95c030cd96ff91ab6e041ec560
MajdHarbb <majdwalidharb@gmail.com>
2022-03-17 18:33:17 +0200

    patient signup textfields

--- lib/signIn_signup/patient_signup.dart
+++ lib/signIn_signup/patient_signup.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class PatientSignIn extends StatefulWidget {
-  const PatientSignIn({ Key? key }) : super(key: key);
+  const PatientSignIn({Key? key}) : super(key: key);
 
   @override
   State<PatientSignIn> createState() => _PatientSignInState();
@@ -10,9 +10,64 @@ class PatientSignIn extends StatefulWidget {
 class _PatientSignInState extends State<PatientSignIn> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:
-      Text('sd'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Patient Sign Up!'),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter your username',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter your username',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter your username',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter your username',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Enter your username',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
