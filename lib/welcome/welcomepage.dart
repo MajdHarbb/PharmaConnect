e00f8e41f5e5b723c1b53f7b08867d4d630ea70a
MajdHarbb <majdwalidharb@gmail.com>
2022-03-26 03:25:35 +0200

    welcome page 2 frinal design

--- lib/welcome/welcomepage.dart
+++ lib/welcome/welcomepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pharmaconnectflutter/welcome/patient_or_pharmacy.dart';
 import 'package:pharmaconnectflutter/welcome/welcome_signin.dart';
 
 class WelcomePage extends StatelessWidget {
@@ -10,7 +9,7 @@ class WelcomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only( left: 15.0, top: 50, right: 15.0, bottom: 80),
+          padding: const EdgeInsets.only( left: 15.0, top: 50, right: 15.0, bottom: 40),
           child: Center(
             
             child: Column(
@@ -26,13 +25,21 @@ class WelcomePage extends StatelessWidget {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => const PatientOrPharmacy()),
+                                builder: (context) => const WelcomeSignIn()),
                           );
                           
                         },
                         child: const Text("Skip")),
                         const Icon(Icons.skip_next_sharp, color: Colors.blue),
                   ],
+                ),
+                const Text(
+                  'Welcome To PharmaConnect!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 35,
+                      letterSpacing: 1,
+                      fontWeight: FontWeight.bold),
                 ),
                 Image.asset('assets/images/pharmacy.png'),
 
@@ -82,36 +89,75 @@ class WelcomePagetwo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Route wl'),
-      ),
-      body: Center(
-        
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-           
-            Image.asset('assets/images/logo.png'),
+     return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only( left: 15.0, top: 20, right: 15.0, bottom: 40),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const WelcomeSignIn()),
+                          );
+                          
+                        },
+                        child: const Text("Skip")),
+                        const Icon(Icons.skip_next_sharp, color: Colors.blue),
+                  ],
+                ),
+                const Text(
+                  'Welcome To PharmaConnect!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 35,
+                      letterSpacing: 1,
+                      fontWeight: FontWeight.bold),
+                ),
+                Image.asset('assets/images/logo.png'),
 
-            const Text(
-              'Post about any medicine and know it’s location',
+                const Text(
+                  'Post about any medicine and know it’s location',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 30,
+                      letterSpacing: 1,
+                      fontWeight: FontWeight.bold),
+                ),
+
+                const Text(
+                  'Create a post, add a picture of the medicine you want, and get a reply from the pharmacy that has your medicine!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 15, letterSpacing: 1),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const WelcomeSignIn()),
+                          );
+                          
+                        },
+                        child: const Text("Next")),
+                        const Icon(Icons.skip_next_sharp, color: Colors.blue),
+                  ],
+                ),
+                
+              ],
             ),
-            const Text(
-              'Now you can find your medicine with few clicks, no need for driving across the country anymore!',
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const WelcomeSignIn()),
-                );
-              },
-              child: const Text('Next    >'),
-            ),
-          ],
         ),
       ),
     );
