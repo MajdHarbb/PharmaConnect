e292ef0cb0c16a2138fe34a8a33d626e8d396295
MajdHarbb <majdwalidharb@gmail.com>
2022-03-27 03:04:28 +0300

    notifications basic layout

--- lib/patient/notifications.dart
+++ lib/patient/notifications.dart
@@ -1,12 +1,9 @@
-// main.dart
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:pharmaconnectflutter/models/botnav.dart';
+import 'package:pharmaconnectflutter/map/map_utils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
-import 'dart:convert'; // for using json.decode()
-
-
+import 'dart:convert';
 class Notifications extends StatefulWidget {
   const Notifications({Key? key}) : super(key: key);
 
@@ -15,7 +12,8 @@ class Notifications extends StatefulWidget {
 }
 
 class _NotificationsState extends State<Notifications> {
-  // The list that contains information about photos
+  @override
+    // The list that contains information about photos
   List _loadedPhotos = [];
   String access_Token = "";
 
@@ -47,43 +45,158 @@ class _NotificationsState extends State<Notifications> {
     super.initState();
     print(
         "hello =========================================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>;d");
-    //_fetchData();
+    _fetchData();
   }
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Kindacode.com'),
+        body: CustomScrollView(
+      slivers: [
+        // const SliverAppBar(
+        //   backgroundColor: Colors.amber,
+        //   title: Text('Kindacode.com'),
+        //   expandedHeight: 30,
+        //   collapsedHeight: 150,
+        // ),
+        const SliverAppBar(
+          //backgroundColor: Colors.green,
+          title: Text('Pharmacies'),
+          floating: true,
         ),
-        body: SafeArea(
-            child: _loadedPhotos.length == 0
-                ? Center(
-                    child: ElevatedButton(
-                      child: Text('Load Photos'),
-                      onPressed: (){
-                        Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const MyApp()));
-                      },
-                    ),
-                  )
-                // The ListView that displays photos
-                : ListView.builder(
-                    itemCount: _loadedPhotos.length,
-                    itemBuilder: (BuildContext ctx, index) {
-                      return ListTile(
-                        leading: Text(
-                          _loadedPhotos[index]["district"],
-                          //width: 150,
-                          //fit: BoxFit.cover,
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, int index) {
+              return Container(
+                margin: const EdgeInsets.all(15),
+                padding: const EdgeInsets.all(8.0),
+                color: Colors.white,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 30.0,
+                          backgroundColor: Colors.grey[200],
+                          //backgroundImage: NetworkImage("assets\images\test.jpg"),
                         ),
-                        
-                        title: Text(_loadedPhotos[index]['district']),
-                        subtitle:
-                            Text('Photo ID: ${_loadedPhotos[index]["district"]}'),
-                      );
-                    },
-                  ))
-         );
+                        const SizedBox(width: 8.0),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                _loadedPhotos[index]["name"],
+                                style: const TextStyle(fontWeight: FontWeight.w600),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    "1 hour ago",
+                                    style: TextStyle(
+                                      color: Colors.grey[600],
+                                      fontSize: 12.0,
+                                    ),
+                                  ),
+                                  Icon(
+                                    Icons.public,
+                                    color: Colors.grey[600],
+                                    size: 12.0,
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.more_horiz),
+                          onPressed: () {
+                            print('more');
+                          },
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 4.0),
+                    const Text("Post Caption"),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Image.asset(
+                        'assets/images/aspirin.jpg',
+                        width: 600.0,
+                        height: 240.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton.icon(
+                          icon: const Icon(
+                            Icons.remove_red_eye_outlined,
+                            color: Colors.blue,
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(10, 40),
+                            primary: Colors.white,
+                          ),
+
+                          onPressed: () {
+                            MapUtils.openMap(-55.42657, -93.39159);
+                          },
+                          label: const Text(
+                            'View',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          //controller: streetController,
+                        ),
+                        ElevatedButton.icon(
+                          icon: const Icon(
+                            Icons.phone,
+                            color: Colors.blue,
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(10, 40),
+                            primary: Colors.white,
+                          ),
+
+                          onPressed: () {
+                            print("test");
+                          },
+                          label: const Text(
+                            'Call',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          //controller: streetController,
+                        ),
+                        ElevatedButton.icon(
+                          icon: const Icon(
+                            Icons.share,
+                            color: Colors.blue,
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(10, 40),
+                            primary: Colors.white,
+                          ),
+
+                          onPressed: () {
+                            print("test");
+                          },
+                          label: const Text(
+                            'Share',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          //controller: streetController,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            },
+            childCount: _loadedPhotos.length, // 1000 list items
+          ),
+        ),
+      ],
+    )
+    );
   }
 }
