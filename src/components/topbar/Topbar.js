40e32912e2e1db17d00547735c9ab93983e34c34
MajdHarbb <majdwalidharb@gmail.com>
2022-03-28 17:25:34 +0300

    top nav design

--- src/components/topbar/Topbar.js
+++ src/components/topbar/Topbar.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import "../../css/Topbar.css";
+import "../../assets/profile_pictures/test.jpg"
+import { NotificationsNone , Language, Settings} from "@material-ui/icons";
 
 export default function Topbar() {
   return (
-    <div className='topbar'>
-        <div className='topbarWrapper'>
-            <div className='topLeft'>
-                <span className='logo'>PharmaConnect</span>
-            </div>
-            <div className='topLeft'>right</div>
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">PharmaConnect</span>
         </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img src={require('../../assets/profile_pictures/test.jpg')} alt="" className="topAvatar" />
+        </div>
+      </div>
     </div>
   )
 }
