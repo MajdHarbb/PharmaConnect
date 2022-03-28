87ce58d58fd6fbc7e20ff11020363b7264b86496
MajdHarbb <majdwalidharb@gmail.com>
2022-03-28 04:12:32 +0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
-import {LandingPage} from "./pages/landing";
+import LandingPage from "./pages/landing";
+import LoginPage from "./pages/LoginPage";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+
 function App() {
   return (
     <div className="App">
       <h1>Protected React Router</h1>
+      {/* <Route component = {LandingPage}/> */}
       <LandingPage/>
+      <LoginPage/>
     </div>
   );
 }
