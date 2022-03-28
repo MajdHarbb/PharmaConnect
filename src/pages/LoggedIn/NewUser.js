92c0ae89c7482d147cbad1d996fc2441cc5338b3
MajdHarbb <majdwalidharb@gmail.com>
2022-03-29 02:23:43 +0300

    user page

--- src/pages/LoggedIn/NewUser.js
+++ src/pages/LoggedIn/NewUser.js
@@ -21,8 +21,21 @@ export default function NewUser() {
           <label>Password</label>
           <input type="password" placeholder="password" />
         </div>
-      <div>
-          
+        <div className="newUserItem">
+          <label>Phone</label>
+          <input type="text" placeholder="+1 123 456 78" />
+        </div>
+        <div className="newUserItem">
+          <label>Address</label>
+          <input type="text" placeholder="New York | USA" />
+        </div>
+
+        <div className="newUserItem">
+          <label>Active</label>
+          <select className="newUserSelect" name="active" id="active">
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
         </div>
         <button className="newUserButton">Create</button>
       </form>
