b2b2df7ac93acded83557f1450ba4448ef33841d
MajdHarbb <majdwalidharb@gmail.com>
2022-03-29 02:21:21 +0300

    create user page

---
+++ src/pages/LoggedIn/NewUser.js
@@ -0,0 +1,31 @@
+import "./newUser.css";
+
+export default function NewUser() {
+  return (
+    <div className="newUser">
+      <h1 className="newUserTitle">New User</h1>
+      <form className="newUserForm">
+        <div className="newUserItem">
+          <label>Username</label>
+          <input type="text" placeholder="john" />
+        </div>
+        <div className="newUserItem">
+          <label>Full Name</label>
+          <input type="text" placeholder="John Smith" />
+        </div>
+        <div className="newUserItem">
+          <label>Email</label>
+          <input type="email" placeholder="john@gmail.com" />
+        </div>
+        <div className="newUserItem">
+          <label>Password</label>
+          <input type="password" placeholder="password" />
+        </div>
+      <div>
+          
+        </div>
+        <button className="newUserButton">Create</button>
+      </form>
+    </div>
+  );
+}
