77662850d8dc9bac7460186ee360319723a80281
MajdHarbb <majdwalidharb@gmail.com>
2022-03-28 19:05:26 +0300

    chart

--- src/components/topbar/Chart.js
+++ src/components/topbar/Chart.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import "../../css/Chart.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
-const Chart = () => {
-    return (
-        <div>
-            Chart 
-        </div>
-    );
-};
-
-export default Chart;
+export default function Chart({ title, data, dataKey, grid }) {
+  return (
+    <div className="chart">
+      <h3 className="chartTitle">{title}</h3>
+      <ResponsiveContainer width="100%" aspect={4 / 1}>
+        <LineChart data={data}>
+          <XAxis dataKey="name" stroke="#5550bd" />
+          <Line type="monotone" dataKey={dataKey} stroke="#5550bd" />
+          <Tooltip />
+          {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="5 5" />}
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
