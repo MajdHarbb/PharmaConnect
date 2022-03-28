5b5a5c0e6cfd989e4d3acffff2f7486d0ec8b534
MajdHarbb <majdwalidharb@gmail.com>
2022-03-28 18:52:58 +0300

    chart

--- src/pages/LoggedIn/Home.js
+++ src/pages/LoggedIn/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Chart from '../../components/topbar/Chart';
 import FeaturedInfo from '../../components/topbar/FeaturedInfo';
 
 const Home = () => {
     return (
         <div className='home'>
             <FeaturedInfo/>
+            <Chart/>
         </div>
     );
 };
