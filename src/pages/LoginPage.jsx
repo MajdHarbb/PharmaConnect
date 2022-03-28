8daa80d1cdcf9f1c09bace0e50aae6c62cf74ad6
MajdHarbb <majdwalidharb@gmail.com>
2022-03-28 08:36:04 +0300

    axios fetch login api

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,27 +1,34 @@
 import React, { useState, useEffect } from "react";
 import "../css/login.css";
 import "../js/index.js";
+import axios from 'axios';
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  var x = "";
+  var y = "";
   useEffect(() => {
     const form = document.getElementById("form");
     var eField = document.getElementById("field-email");
     var eInput = document.getElementById("input-email");
     var pField = document.getElementById("field-password");
     var pInput = document.getElementById("input-password");
-    console.log(eField);
+    x = eInput.value;
+    y=pInput.value;
     form.onsubmit = (e) => {
       e.preventDefault(); //preventing from form submitting
-      //if email and password is blank then add shake class in it else call specified function
       eInput.value == ""
-        ? eField.classList.add("shake", "error")
+        ? eField.classList.add("error")
         : checkEmail();
-      pInput.value == "" ? pField.classList.add("shake", "error") : checkPass();
+      pInput.value == "" ? pField.classList.add("error") : checkPass();
 
       setTimeout(() => {
-        //remove shake class after 500ms
-        eField.classList.remove("shake");
-        pField.classList.remove("shake");
-      }, 500);
+        //remove error class after 500ms
+        eField.classList.remove("error");
+        pField.classList.remove("error");
+      }, 2000);
 
       eInput.onkeyup = () => {
         checkEmail();
@@ -71,8 +78,50 @@ const LoginPage = () => {
       }
     };
   });
-  function fetchLoginApi() {
-    console.log("loggedin");
+
+  var postData = {
+    email: "user@ojne.com",
+    password: "user123456"
+  };
+  
+  let axiosConfig = {
+    headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+    }
+  };
+  console.log(postData)
+  async function  fetchLoginApi() {
+    axios.post(`http://127.0.0.1:8000/api/auth/login`,
+    {email: x,password: y},axiosConfig)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        console.log(x,y);
+        console.log(email);
+      });
+      //fetch login API: checks if user exists in the database and returns a JWT token
+      // const response = await fetch("http://127.0.0.1:8000/api/auth/login", {
+
+      //   method: "POST",
+
+      //   headers: { "Content-Type": "application/json" },
+
+      //   body: JSON.stringify({
+
+      //     email: x,
+      //     password: y,
+      //   }),
+      // });
+      // const content = await response.json();
+
+      // //if user is found store token in the local storage
+      // if (response.status != 401) {
+      //   console.log(content.access_token)
+      // }
+      // console.log(response);
+      //   console.log(x,y);
+      //   console.log(email);
+
   }
   return (
     <div>
@@ -81,13 +130,13 @@ const LoginPage = () => {
         <form id="form">
           <div className="field email" id="field-email">
             <div className="input-area">
-              <input type="text" placeholder="Email Address" id="input-email" />
+              <input type="text" placeholder="Email Address" id="input-email" onChange={(e) => setEmail(e.target.value)}/>
               <i className="icon fas fa-envelope" />
               <i className="error error-icon fas fa-exclamation-circle" />
             </div>
             <div className="error error-txt">Email can't be blank</div>
           </div>
-          <div className="field password" id="field-password">
+          <div className="field password" id="field-password" onChange={(e) => setPassword(e.target.value)}>
             <div className="input-area">
               <input
                 type="password"
