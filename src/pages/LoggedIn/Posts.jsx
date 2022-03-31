6fc2aa8278d27c52e7973a55fe49e59a35323000
MajdHarbb <majdwalidharb@gmail.com>
2022-03-31 15:08:00 +0300

    card posts

--- src/pages/LoggedIn/Posts.jsx
+++ src/pages/LoggedIn/Posts.jsx
@@ -30,49 +30,63 @@ const Posts = () => {
   }, []);
   if(isLoading){
   return (
-    <div className="main-div">
-        <h2>Solved Posts:</h2>
+  <div className="wrapperr">
+  <div className="header">Pharma Connect Solved Posts</div>
+  <div className="cards_wrap">
       {posts.map((singlePost) => {
         return (
             
-          <div id="posts-div">
+          // <div id="posts-div">
               
-            <div className="post">
-              <div className="post-top">
-                <div className="dp">
-                  <img src={require("../../assets/profiles/"+singlePost.profile_pic)} alt />
-                </div>
-                <div className="post-info">
-                  <a href="#">
-                    <p className="name">{singlePost.name}</p>
-                  </a>
-                  <span className="time">{singlePost.updated_at}</span>
-                </div>
-                <div className="delete">
-                  <DeleteRoundedIcon/>
-                </div>
-              </div>
-              <div className="post-content">
-              {singlePost.post_text}
-              {/* <img src={require("../../assets/posts/"+singlePost.post_pic)}
-                alt="" /> */}
+          //   <div className="post">
+          //     <div className="post-top">
+          //       <div className="dp">
+          //         <img src={require("../../assets/profiles/"+singlePost.profile_pic)} alt />
+          //       </div>
+          //       <div className="post-info">
+          //         <a href="#">
+          //           <p className="name">{singlePost.name}</p>
+          //         </a>
+          //         <span className="time">{singlePost.updated_at}</span>
+          //       </div>
+          //       <div className="delete">
+          //         <DeleteRoundedIcon/>
+          //       </div>
+          //     </div>
+          //     <div className="post-content">
+          //     {singlePost.post_text}
+          //     {/* <img src={require("../../assets/posts/"+singlePost.post_pic)}
+          //       alt="" /> */}
               
-              </div>
-              <div className="post-bottom">
-                <div className="action">
-                  <Check/>
-                  <span>Solved </span>
-                </div>
-                {/* <div className="action">
-                  <i className="far fa-heart" />
-                  <span>Love</span>
-                </div> */}
-              </div>
-            </div>
-          </div>
+          //     </div>
+          //     <div className="post-bottom">
+          //       <div className="action">
+          //         <Check/>
+          //         <span>Solved </span>
+          //       </div>
+          //       {/* <div className="action">
+          //         <i className="far fa-heart" />
+          //         <span>Love</span>
+          //       </div> */}
+          //     </div>
+          //   </div>
+          // </div>
+<div className="card_item">
+  <div className="card_inner">
+    <img src="post" />
+    <div className="name">test</div>
+    <div className="date">test</div>
+    <div className="post-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.</div>
+  </div>
+</div>
+
+
+
         );
       })}
-    </div>
+        </div>
+</div>
   );
     }else{
       return(
