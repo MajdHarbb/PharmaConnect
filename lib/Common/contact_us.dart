cd791d56f6d4da5096cf6e431ff1962093321108
MajdHarbb <majdwalidharb@gmail.com>
2022-04-03 02:51:20 +0300

    msg

--- lib/Common/contact_us.dart
+++ lib/Common/contact_us.dart
@@ -11,7 +11,7 @@ class ContactUs extends StatefulWidget {
 
 class _ContactUsState extends State<ContactUs> {
   final _formKey = GlobalKey<FormState>();
-  final TextEditingController messageController = TextEditingController();
+   TextEditingController messageController = TextEditingController();
 
   String user_id = "";
   String access_Token = "";
@@ -33,7 +33,7 @@ class _ContactUsState extends State<ContactUs> {
               ),
               Container(
                 margin: const EdgeInsets.all(15),
-                padding: const EdgeInsets.all(8.0),
+                // padding: const EdgeInsets.all(8.0),
                 decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: const BorderRadius.only(
@@ -55,6 +55,7 @@ class _ContactUsState extends State<ContactUs> {
                   child: Column(
                     children: [
                       TextFormField(
+                        textAlignVertical: TextAlignVertical.top,
                         validator: (value) {
                           if (value == null || value.isEmpty) {
                             return 'Please enter some text';
@@ -66,64 +67,84 @@ class _ContactUsState extends State<ContactUs> {
                         maxLines: 5,
                         controller: messageController,
                         decoration: const InputDecoration(
-                          prefixIcon: Icon(Icons.message_rounded),
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.only(bottom: 85),
+                            child: Icon(Icons.message_rounded),
+                          ),
+                          alignLabelWithHint: true,
+                          
                           border: OutlineInputBorder(),
                           labelText: 'Your Message Text Here...',
+                          
                         ),
                       ),
-                      Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                        TextButton(
-                          onPressed: () async {
-                            if (_formKey.currentState!.validate()) {
-                              final response = await http.post(
-                                Uri.parse(
-                                    'http://192.168.0.117:8000/api/user/send-message'),
-                                headers: {
-                                  'Authorization': 'Bearer $access_Token',
-                                },
-                                body: {
-                                  'user_id': user_id,
-                                  'message_text': messageController.text,
-                                },
-                              );
-
-                              if (response.statusCode == 201) {
-                                // If the server did return a 201 CREATED response,
-                                // then parse the JSON.
-                                showDialog<String>(
-                                  context: context,
-                                  builder: (BuildContext context) =>
-                                      AlertDialog(
-                                    title: const Text(
-                                        'Thank You! We Received Your Message.'),
-                                    content: const Text(
-                                        'Press okay to return to your screen'),
-                                    actions: <Widget>[
-                                      TextButton(
-                                        onPressed: () =>
-                                            Navigator.pop(context, 'OK'),
-                                        child: const Text('OK'),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              } else {
-                                // If the server did not return a 201 CREATED response,
-                                // then throw an exception.
-                                print(response.body);
-                              }
-                            }
-                          },
-                          child: const Text('Send Message'),
-                        ),
-                        const Icon(
-                          Icons.send_rounded,
-                          color: Colors.blue,
-                        ),
-                      ]),
                     ],
                   ),
                 ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (_formKey.currentState!.validate()) {
+                        final response = await http.post(
+                          Uri.parse(
+                              'http://192.168.0.117:8000/api/user/send-message'),
+                          headers: {
+                            'Authorization': 'Bearer $access_Token',
+                          },
+                          body: {
+                            'user_id': user_id,
+                            'message_text': messageController.text,
+                          },
+                        );
+
+                        if (response.statusCode == 201) {
+                          // If the server did return a 201 CREATED response,
+                          // then parse the JSON.
+                          setState(() {
+                            messageController =TextEditingController();
+                          });
+                          showDialog<String>(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                              title: const Text(
+                                  'Thank You! We Received Your Message.'),
+                              content: const Text(
+                                  'Press okay to return to your screen'),
+                              actions: <Widget>[
+                                TextButton(
+                                  onPressed: () => Navigator.pop(context, 'OK'),
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            ),
+                            
+                          );
+                        } else {
+                          // If the server did not return a 201 CREATED response,
+                          // then throw an exception.
+                          print(response.body);
+                        }
+                      }
+                    },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Text('Send Message'), // <-- Text
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Icon(
+                          // <-- Icon
+                          Icons.send_rounded,
+                          size: 24.0,
+                        ),
+                      ],
+                    ),
+                  ),
+                ]),
               ),
             ],
           ),
