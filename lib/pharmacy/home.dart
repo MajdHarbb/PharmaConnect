9fca0c7f81ed72290fb75bb68d20aa28cc8edb86
MajdHarbb <majdwalidharb@gmail.com>
2022-04-03 03:46:48 +0300

    visibility: hide post

--- lib/pharmacy/home.dart
+++ lib/pharmacy/home.dart
@@ -16,7 +16,7 @@ class _PharmacyHomeState extends State<PharmacyHome> {
   List _loadedPhotos = [];
   String access_Token = "";
   String user_id = "";
-
+  bool isVisible = true;
   // The function that fetches data from the API
   Future<void> _fetchData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -118,90 +118,121 @@ class _PharmacyHomeState extends State<PharmacyHome> {
           delegate: SliverChildBuilderDelegate(
             (context, int index) {
               return Container(
-                margin: const EdgeInsets.all(15),
-                padding: const EdgeInsets.all(8.0),
-                color: Colors.white,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Row(
+                  margin: const EdgeInsets.all(15),
+                  padding: const EdgeInsets.all(8.0),
+                  color: Colors.white,
+                  child: Visibility(
+                    visible: isVisible,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
-                        CircleAvatar(
-                          radius: 30.0,
-                          backgroundColor: Colors.grey[200],
-                          backgroundImage: 
-                              NetworkImage('http://192.168.0.117:8000/profiles/${_loadedPhotos[index]["profile_pic"]}'),
-                        ),
-                        const SizedBox(width: 8.0),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                _loadedPhotos[index]["name"],
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.w600),
-                              ),
-                              Row(
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 30.0,
+                              backgroundColor: Colors.grey[200],
+                              backgroundImage: 
+                                  NetworkImage('http://192.168.0.117:8000/profiles/${_loadedPhotos[index]["profile_pic"]}'),
+                            ),
+                            const SizedBox(width: 8.0),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Text(
-                                    _loadedPhotos[index]["updated_at"].substring(0, _loadedPhotos[index]["updated_at"].indexOf('T')),
-                                    style: TextStyle(
-                                      color: Colors.grey[600],
-                                      fontSize: 12.0,
-                                    ),
+                                    _loadedPhotos[index]["name"],
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600),
                                   ),
-                                  Icon(
-                                    Icons.public,
-                                    color: Colors.grey[600],
-                                    size: 12.0,
-                                  )
+                                  Row(
+                                    children: [
+                                      Text(
+                                        _loadedPhotos[index]["updated_at"].substring(0, _loadedPhotos[index]["updated_at"].indexOf('T')),
+                                        style: TextStyle(
+                                          color: Colors.grey[600],
+                                          fontSize: 12.0,
+                                        ),
+                                      ),
+                                      Icon(
+                                        Icons.public,
+                                        color: Colors.grey[600],
+                                        size: 12.0,
+                                      )
+                                    ],
+                                  ),
                                 ],
                               ),
-                            ],
-                          ),
+                            ),
+                            
+                          ],
                         ),
-                        
-                      ],
-                    ),
-                    const SizedBox(height: 4.0),
-                    Text(_loadedPhotos[index]["post_text"]),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: 
-                      
-                      Image.network('http://192.168.0.117:8000/posts/${_loadedPhotos[index]["post_pic"]}',width: 600.0,
-                        height: 240.0,
-                        fit: BoxFit.cover,),
-                      
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ElevatedButton.icon(
-                          icon: const Icon(
-                            Icons.check_circle_outline_rounded,
-                            color: Colors.blue,
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: const Size(80.0, 60.0),
-                            primary: Colors.white,
-                          ),
+                        const SizedBox(height: 4.0),
+                        Text(_loadedPhotos[index]["post_text"]),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: 
+                          
+                          Image.network('http://192.168.0.117:8000/posts/${_loadedPhotos[index]["post_pic"]}',width: 600.0,
+                            height: 240.0,
+                            fit: BoxFit.cover,),
+                          
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
 
-                          onPressed: () {
-                            SolvePost((_loadedPhotos[index]["id"]).toString(),(_loadedPhotos[index]["user_id"].toString()));
-                          },
-                          label: const Text(
-                            'Send Availability Message',
-                            style: TextStyle(color: Colors.black),
-                          ),
-                          //controller: streetController,
-                        ),
+                            ElevatedButton.icon(
+                              icon: const Icon(
+                                Icons.delete_outline_rounded,
+                                color: Colors.red,
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: const Size(80.0, 60.0),
+                                primary: Colors.white,
+                                elevation: 0.0,
+                                shadowColor: Colors.transparent,
+                                
+                              ),
+
+                              onPressed: () {
+                                setState(() {
+                                  isVisible =!isVisible;
+                                });
+                              },
+                              label: const Text(
+                                'Ignore',
+                                style: TextStyle(color: Colors.red),
+                              ),
+                              //controller: streetController,
+                            ),
+                            ElevatedButton.icon(
+                              icon: const Icon(
+                                Icons.check_circle_outline_rounded,
+                                color: Colors.green,
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                minimumSize: const Size(80.0, 60.0),
+                                primary: Colors.white,
+                                elevation: 0.0,
+                                shadowColor: Colors.transparent,
+                              ),
+
+                              onPressed: () {
+                                SolvePost((_loadedPhotos[index]["id"]).toString(),(_loadedPhotos[index]["user_id"].toString()));
+                              },
+                              label: const Text(
+                                'Available',
+                                style: TextStyle(color: Colors.green),
+                              ),
+                              //controller: streetController,
+                            ),
+                          ],
+                        )
                       ],
-                    )
-                  ],
-                ),
-              );
+                    ),
+                  ),
+                );
+              
             },
             childCount: _loadedPhotos.length, // 1000 list items
           ),
