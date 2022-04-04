f6fd5c55a7c3609ec21fa001c957f060cbe011cd
MajdHarbb <majdwalidharb@gmail.com>
2022-04-04 03:22:23 +0300

    pharmacies

--- lib/patient/SolvedPost.dart
+++ lib/patient/SolvedPost.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pharmaconnectflutter/map/map_utils.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -21,9 +22,159 @@ class _SolvedPostState extends State<SolvedPost> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: 
-      Text(widget.postid),)
-    );
+        body: CustomScrollView(
+      slivers: [
+        const SliverAppBar(
+          title: Text('Pharmacies'),
+          floating: true,
+        ),
+        SliverList(
+          
+          delegate: SliverChildBuilderDelegate(
+            (context, int index) {
+              return Container(
+                margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(8),
+                      topRight: Radius.circular(8),
+                      bottomLeft: Radius.circular(8),
+                      bottomRight: Radius.circular(8)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                //color: Colors.white,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 30.0,
+                          backgroundColor: Colors.grey[200],
+                          backgroundImage: 
+                              NetworkImage('http://192.168.0.117:8000/profiles/${_loadedPhotos[index]["profile_pic"]}'),
+
+                        ),
+                        const SizedBox(width: 8.0),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                _loadedPhotos[index]["name"],
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.w600),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    'member since ${_loadedPhotos[index]["updated_at"].substring(0, _loadedPhotos[index]["updated_at"].indexOf('T'))}',
+                                    style: TextStyle(
+                                      color: Colors.grey[600],
+                                      fontSize: 12.0,
+                                    ),
+                                  ),
+                                  Icon(
+                                    Icons.public,
+                                    color: Colors.grey[600],
+                                    size: 12.0,
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 4.0),
+                    Text(
+                        '${_loadedPhotos[index]["building"]}, ${_loadedPhotos[index]["street"]}'),
+                    Text('${_loadedPhotos[index]["district"]}'),
+                    Text('${_loadedPhotos[index]["locality"]}'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton.icon(
+                          icon: const Icon(
+                            Icons.location_on,
+                            color: Colors.blue,
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(10, 40),
+                            primary: Colors.white,
+                          ),
+
+                          onPressed: () {
+                            MapUtils.openMap(_loadedPhotos[index]["latitude"],
+                                _loadedPhotos[index]["longitude"]);
+                          },
+                          label: const Text(
+                            'Maps',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          //controller: streetController,
+                        ),
+                        ElevatedButton.icon(
+                          icon: const Icon(
+                            Icons.phone,
+                            color: Colors.blue,
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(10, 40),
+                            primary: Colors.white,
+                          ),
+
+                          onPressed: () {
+                            MapUtils.openDialer(
+                                '${_loadedPhotos[index]["phone"]}');
+                          },
+                          label: const Text(
+                            'Call',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          //controller: streetController,
+                        ),
+                        ElevatedButton.icon(
+                          icon: const Icon(
+                            Icons.email_rounded,
+                            color: Colors.blue,
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(10, 40),
+                            primary: Colors.white,
+                          ),
+
+                          onPressed: () {
+                            MapUtils.openMail(
+                                '${_loadedPhotos[index]["email"]}');
+                          },
+                          label: const Text(
+                            'Email',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          //controller: streetController,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            },
+            childCount: _loadedPhotos.length,
+            // 1000 list items
+          ),
+        ),
+      ],
+    ));
   }
 
 
@@ -39,7 +190,7 @@ class _SolvedPostState extends State<SolvedPost> {
     setState(() {
       access_Token = prefs.getString('accesToken')!;
     });
-    const API_URL = 'http://192.168.0.117:8000/api/user/get-pharmacies';
+    final API_URL = 'http://192.168.0.117:8000/api/user/pharmacy-by-post?post_id=${widget.postid}';
 
     final response = await http.get(
       Uri.parse(API_URL),
@@ -54,5 +205,11 @@ class _SolvedPostState extends State<SolvedPost> {
       _loadedPhotos = data;
       print(_loadedPhotos[1]["district"]);
     });
+  }
+
+  @override
+  initState(){
+    super.initState();
+    fetchPharmacies();
   }
 }
