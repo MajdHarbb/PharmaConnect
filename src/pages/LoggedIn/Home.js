241fee78de2b4ef51b649e0aee3cac613ef87a2e
MajdHarbb <majdwalidharb@gmail.com>
2022-04-05 03:03:39 +0300

    remove widgets

--- src/pages/LoggedIn/Home.js
+++ src/pages/LoggedIn/Home.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Chart from "../../components/topbar/Chart";
 import FeaturedInfo from "../../components/topbar/FeaturedInfo";
-import WidgetLg from "../../components/topbar/WidgetLg";
-import WidgetSm from "../../components/topbar/WidgetSm";
 import "../../css/Home.css";
 const Home = () => {
   return (
