469b5bf5f655937e105e2193258e081e8eb35f3a
MajdHarbb <majdwalidharb@gmail.com>
2022-04-06 19:06:50 +0300

    about us

--- lib/Common/about_us.dart
+++ lib/Common/about_us.dart
@@ -1,10 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:async';
-import 'package:http/http.dart' as http;
+
 
 class AboutUs extends StatefulWidget {
   const AboutUs({Key? key}) : super(key: key);
@@ -14,65 +9,10 @@ class AboutUs extends StatefulWidget {
 }
 
 class _AboutUsState extends State<AboutUs> {
-  List _loadedPhotos = [];
-  String access_Token = "";
-  String pharmacy_profile_pic = "";
-  List<Marker> _markers =[];
-  List<Marker> _list =[
-    Marker(markerId: MarkerId('jgasbdhkj'),
-    position: LatLng(38.123,35.123))
-  ];
-  Future<void> _fetchData() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    //Return String
-    String? stringValue = prefs.getString('accesToken');
-
-    setState(() {
-      access_Token = prefs.getString('accesToken')!;
-    });
-    const API_URL = 'http://192.168.0.117:8000/api/user/get-pharmacies';
-
-    final response = await http.get(
-      Uri.parse(API_URL),
-      headers: {
-        'Authorization': 'Bearer $access_Token',
-      },
-    );
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      final data = json.decode(response.body);
-      print("aa3333$data");
-      
-      print("amjds");
-      setState(() {
-        _loadedPhotos = data;
-        // allMarkers=_markers;
-        print("a3333333333333${_loadedPhotos[0]["building"]}");
-        for (int i = 0; i < data.length; i++) {
-        LatLng latlng = LatLng(double.parse(data[i]["latitude"].toString()),double.parse(data[i]["longitude"].toString()));
-        _list.add(Marker(
-              markerId: MarkerId(data[i]["id"].toString()),
-              position: latlng,
-            ));
-        print("hes${data[i]["latitude"]}");
-        print("hes${data[i]["longitude"]}");
-        print("hes$_list");
-        
-      }
-      _markers.addAll(_list);
-      });
-    }
-  }
-
-  LatLng latlng = LatLng(
-    -33.8670522,
-    151.1957362,
-  );
 
   @override
   void initState() {
     super.initState();
-    _fetchData();
-    _markers.addAll(_list);
   }
 
   @override
@@ -81,14 +21,20 @@ class _AboutUsState extends State<AboutUs> {
       appBar: AppBar(
         title: const Text('About Us'),
       ),
-      body: GoogleMap(
-        myLocationEnabled: false,
-        markers: Set<Marker>.of(_markers),
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(32.1749132, 74.1779387),
-          zoom: 5.0,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: const [
+            
+            Text("PharmaConnect is an app to combat medicine shortages in Lebanon. PharmaConnect is more than an app or a practice of code; this app serves a good cause, reduces the impact of the disastrous medicine shortages, and maybe even, saves a soul.",
+            style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.justify),
+            SizedBox(height: 20.0,),
+            Text("PharmaConnect connects medicine seekers with pharmacy owners. For example, instead of driving across the entire country to find your medicine, you can simply register, create a post detailing the medicine that you're looking for ie. with a caption and a picture and you'll get a reply from all the pharmacies that have your requested medicine.",
+            style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.justify),
+
+          ],
         ),
-      ),
+      )
     );
   }
 }
