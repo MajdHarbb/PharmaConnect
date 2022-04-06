c5d88009bbece6b5c9666bd5d3f442b2ef8ed221
MajdHarbb <majdwalidharb@gmail.com>
2022-04-06 05:27:48 +0300

    map all markers

--- lib/Common/about_us.dart
+++ lib/Common/about_us.dart
@@ -17,10 +17,11 @@ class _AboutUsState extends State<AboutUs> {
   List _loadedPhotos = [];
   String access_Token = "";
   String pharmacy_profile_pic = "";
-  List<Marker> allMarkers = [];
-  List<Marker> _markers = [];
-
-  // The function that fetches data from the API
+  List<Marker> _markers =[];
+  List<Marker> _list =[
+    Marker(markerId: MarkerId('jgasbdhkj'),
+    position: LatLng(38.123,35.123))
+  ];
   Future<void> _fetchData() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     //Return String
@@ -40,19 +41,24 @@ class _AboutUsState extends State<AboutUs> {
     if (response.statusCode == 200 || response.statusCode == 201) {
       final data = json.decode(response.body);
       print("aa3333$data");
-      for (int i = 0; i < data.length; i++) {
-        LatLng latlng = new LatLng(double.parse(data[i]["latitude"]),double.parse(data[i]["longitude"]));
-        this._markers.add(Marker(
-              markerId: MarkerId(data[i]["id"]),
-              position: latlng,
-            ));
-        print("hes${data[i]["longitude"]}");
-      }
+      
       print("amjds");
       setState(() {
         _loadedPhotos = data;
-        allMarkers=_markers;
+        // allMarkers=_markers;
         print("a3333333333333${_loadedPhotos[0]["building"]}");
+        for (int i = 0; i < data.length; i++) {
+        LatLng latlng = LatLng(double.parse(data[i]["latitude"].toString()),double.parse(data[i]["longitude"].toString()));
+        _list.add(Marker(
+              markerId: MarkerId(data[i]["id"].toString()),
+              position: latlng,
+            ));
+        print("hes${data[i]["latitude"]}");
+        print("hes${data[i]["longitude"]}");
+        print("hes$_list");
+        
+      }
+      _markers.addAll(_list);
       });
     }
   }
@@ -61,33 +67,23 @@ class _AboutUsState extends State<AboutUs> {
     -33.8670522,
     151.1957362,
   );
-  @override
-  //List<Marker> list = [];
 
   @override
   void initState() {
     super.initState();
     _fetchData();
-    LatLng latlng = new LatLng(double.parse("38.123"), double.parse("35.123"));
-    _markers.add(Marker(
-        markerId: MarkerId('SomeId'),
-        position: latlng,
-        infoWindow: InfoWindow(title: 'The title of the marker')));
-
-    setState(() {});
+    _markers.addAll(_list);
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('About Us'),
       ),
       body: GoogleMap(
-        // myLocationEnabled: false,
-        markers: Set<Marker>.of(allMarkers),
-        // onMapCreated: (GoogleMapController controller) {
-        //   _controller.complete(controller);
-        // },
+        myLocationEnabled: false,
+        markers: Set<Marker>.of(_markers),
         initialCameraPosition: const CameraPosition(
           target: LatLng(32.1749132, 74.1779387),
           zoom: 5.0,
