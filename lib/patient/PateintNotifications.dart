1af1b3ec42a91e3fff22689817dd513e06568c02
MajdHarbb <majdwalidharb@gmail.com>
2022-04-10 03:22:15 +0300

    home and notifications

--- lib/patient/PateintNotifications.dart
+++ lib/patient/PateintNotifications.dart
@@ -3,7 +3,6 @@ import 'package:pharmaconnectflutter/patient/SolvedPost.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'dart:async';
 
 class PateintNotifications extends StatefulWidget {
   const PateintNotifications({Key? key}) : super(key: key);
@@ -20,66 +19,16 @@ class _PateintNotificationsState extends State<PateintNotifications> {
   String user_name = "";
   String user_profile_picture = "";
 
-  List _loadedPhotos = [];
+  List _notifications = [];//list to store api notifications
 
   int currentIndex = 0;
-
-  getStringValuesSF() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    //Return String
-    String? stringValue = prefs.getString('accesToken');
-
-    setState(() {
-      user_id = prefs.getString('id')!;
-      user_type = prefs.getString('user_type')!;
-      access_Token = prefs.getString('accesToken')!;
-      user_name = prefs.getString('name')!;
-      user_profile_picture = prefs.getString('profile_pic')!;
-    });
-    const API_URL = 'http://192.168.0.117:8000/api/user/post-pharmacies';
-
-    final response = await http.get(
-      Uri.parse(API_URL),
-      headers: {
-        'Authorization': 'Bearer $access_Token',
-      },
-    );
-    final data = json.decode(response.body);
-    if (response.statusCode == 200) {
-      // If the server did return a 201 CREATED response,
-      // then parse the JSON.
-
-      print(response.body);
-      print("===========> done hh");
-      setState(() {
-        _loadedPhotos = data;
-        count = _loadedPhotos.length;
-      });
-    } else {
-      // If the server did not return a 201 CREATED response,
-      // then throw an exception.
-      print("failed test${response.body}");
-      setState(() {
-        _loadedPhotos = ["no posts yet"];
-        count = 0;
-      });
-    }
-  }
-
-  @override
-  initState() {
-    super.initState();
-    print(
-        "hello =========================================================>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>;d");
-    getStringValuesSF();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Notifications'),
-        leading: Icon(
+        title: const Text('Notifications'),
+        leading: const Icon(
           Icons.notifications_rounded,
         ),
       ),
@@ -92,7 +41,6 @@ class _PateintNotificationsState extends State<PateintNotifications> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    
                     count != 0
                         ? ListView.builder(
                             physics: const NeverScrollableScrollPhysics(),
@@ -161,7 +109,7 @@ class _PateintNotificationsState extends State<PateintNotifications> {
                                                                       (context) =>
                                                                           SolvedPost(
                                                                             postid:
-                                                                                _loadedPhotos[index]["post_id"].toString(),
+                                                                                _notifications[index]["post_id"].toString(),
                                                                           )),
                                                             );
                                                           },
@@ -178,12 +126,12 @@ class _PateintNotificationsState extends State<PateintNotifications> {
                                               Row(
                                                 children: [
                                                   Text(
-                                                    _loadedPhotos[index]
+                                                    _notifications[index]
                                                             ["updated_at"]
                                                         .substring(
                                                             0,
-                                                            _loadedPhotos[index]
-                                                                    [
+                                                            _notifications[
+                                                                        index][
                                                                     "updated_at"]
                                                                 .indexOf('T')),
                                                     style: TextStyle(
@@ -204,12 +152,12 @@ class _PateintNotificationsState extends State<PateintNotifications> {
                                       ],
                                     ),
                                     const SizedBox(height: 4.0),
-                                    Text(_loadedPhotos[index]["post_text"]),
+                                    Text(_notifications[index]["post_text"]),
                                     Padding(
                                       padding: const EdgeInsets.symmetric(
                                           vertical: 8.0),
                                       child: Image.network(
-                                        'http://192.168.0.117:8000/posts/${_loadedPhotos[index]["post_pic"]}?v=${DateTime.now().millisecondsSinceEpoch}',
+                                        'http://192.168.0.117:8000/posts/${_notifications[index]["post_pic"]}?v=${DateTime.now().millisecondsSinceEpoch}',
                                         width: 600.0,
                                         height: 240.0,
                                         fit: BoxFit.cover,
@@ -219,39 +167,83 @@ class _PateintNotificationsState extends State<PateintNotifications> {
                                 ),
                               );
                             },
-                            itemCount: _loadedPhotos.length,
+                            itemCount: _notifications.length,
                             shrinkWrap: true,
                             padding: const EdgeInsets.all(5),
                             scrollDirection: Axis.vertical,
                           )
                         : Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(8),
-                              topRight: Radius.circular(8),
-                              bottomLeft: Radius.circular(8),
-                              bottomRight: Radius.circular(8)),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: const Offset(
-                                  0, 3), // changes position of shadow
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(8),
+                                  topRight: Radius.circular(8),
+                                  bottomLeft: Radius.circular(8),
+                                  bottomRight: Radius.circular(8)),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: const Offset(
+                                      0, 3), // changes position of shadow
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                        margin: const EdgeInsets.all(15),
-                        padding: const EdgeInsets.all(8.0),
-                        child: const Text("You don't have any notifications yet, please wait for a reply"),
-                      ),
-                  ]
-                  ),
+                            margin: const EdgeInsets.all(15),
+                            padding: const EdgeInsets.all(8.0),
+                            child: const Text(
+                                "You don't have any notifications yet, please wait for a reply"),
+                          ),
+                  ]),
             ],
           ),
         ),
       ),
     );
+  }
+
+  getStringValuesSF() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //Return String
+
+    setState(() {
+      user_id = prefs.getString('id')!;
+      user_type = prefs.getString('user_type')!;
+      access_Token = prefs.getString('accesToken')!;
+      user_name = prefs.getString('name')!;
+      user_profile_picture = prefs.getString('profile_pic')!;
+    });
+    const API_URL = 'http://192.168.0.117:8000/api/user/post-pharmacies';
+
+    final response = await http.get(
+      Uri.parse(API_URL),
+      headers: {
+        'Authorization': 'Bearer $access_Token',
+      },
+    );
+    final data = json.decode(response.body);
+    if (response.statusCode == 200) {
+      // If the server did return a 201 CREATED response,
+      // then parse the JSON and set JSON to notifications list
+      setState(() {
+        _notifications = data;
+        count = _notifications.length;
+      });
+    } else {
+      // If the server did not return a 201 CREATED response,
+      // then set count = 0 (display no notifications yet message)
+
+      setState(() {
+        _notifications = ["no notifications yet"];
+        count = 0;
+      });
+    }
+  }
+
+  @override
+  initState() {
+    super.initState();
+    getStringValuesSF();
   }
 }
--- lib/patient/home.dart
+++ lib/patient/home.dart
@@ -18,17 +18,7 @@ class PatientHome extends StatefulWidget {
 }
 
 class _PatientHomeState extends State<PatientHome> {
-  File? image;
-  late String base64_img;
-  String imagePath = '';
-  int count = 0;
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController phoneController = TextEditingController();
-  final TextEditingController passController = TextEditingController();
-  final TextEditingController passConfirmController = TextEditingController();
-  TextEditingController postTextController = TextEditingController();
-  String test = 'hiii';
+
   String user_id = "";
   String user_type = "";
   String access_Token = "";
@@ -37,170 +27,6 @@ class _PatientHomeState extends State<PatientHome> {
   String user_email = "";
   String user_phone = "";
   String user_profile_picture = "";
-  String profiletest = "33.jpeg";
-  Future<void> AddPost() async {
-    final response = await http.post(
-      Uri.parse('http://192.168.0.117:8000/api/user/addPost'),
-      headers: {
-        'Authorization': 'Bearer $access_Token',
-      },
-      body: {
-        'user_id': user_id,
-        'post_text': postTextController.text,
-        'post_pic': "data:image/$extension;base64,$base64_img",
-      },
-    );
-
-    if (response.statusCode == 201) {
-      // If the server did return a 201 CREATED response,
-      // then parse the JSON.
-
-      print(response.body);
-      print("===========> done");
-      image = null;
-      postTextController = TextEditingController();
-      showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title:
-              const Text('Posted! You will get a reply soon from a pharmacy.'),
-          content: const Text('Press okay to return to your screen'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      );
-      getStringValuesSF();
-      setState(() {});
-    } else {
-      // If the server did not return a 201 CREATED response,
-      // then throw an exception.
-      print(response.body);
-    }
-  }
-
-  Future pickImage() async {
-    try {
-      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-      if (image == null) return;
-
-      final imageTemporary = File(image.path);
-      imagePath = image.path;
-      print("---------------------->>>>>>>>-------------------");
-      print(image.path);
-      print("------------------------>>>>>>>>>>>>>>>>>");
-      base64_img = base64Encode(await image.readAsBytes());
-      String imageName = image.path.split("/").last;
-      extension = p.extension(imageName).substring(1);
-      print("extension : $extension");
-      print('image name $imageName');
-      print("baseeeeee $base64_img");
-      setState(() {
-        this.image = imageTemporary;
-        print("picked $imageTemporary");
-      });
-    } on Exception catch (e) {
-      print('Failed to capture image: $e');
-    }
-  }
-
-  Future<void> getUserInfo() async {
-    final prefs = await SharedPreferences.getInstance();
-    final response = await http.get(
-      Uri.parse('http://192.168.0.117:8000/api/user/info?user_id=$user_id'),
-      headers: {
-        'Authorization': 'Bearer $access_Token',
-      },
-    );
-
-    if (response.statusCode == 201) {
-      // If the server did return a 201 CREATED response,
-      // then parse the JSON.
-      var response_jsondata = json.decode(response.body);
-      print("here ======================>>>>>>>>>>>>>>>>>>>>>>>>>>");
-      print(response_jsondata);
-      //var user_json_id = json.decode(response_jsondata);
-      final String nameJson = (response_jsondata["user"][0]["name"]).toString();
-      final String emailJson =
-          (response_jsondata["user"][0]["email"]).toString();
-      final String phoneJson =
-          (response_jsondata["user"][0]["phone"]).toString();
-      final String profileJson =
-          (response_jsondata["user"][0]["profile_pic"]).toString();
-      print("user iddddddddddddddddddddd ===================>>>>>>>>>>");
-      print(nameJson);
-      print(emailJson);
-
-      print(phoneJson);
-      print(profileJson);
-      print("======================>>>>>>>>>>>>>>>>>>>>>>>>>>");
-      print(response.body);
-
-      await prefs.setString('name', nameJson);
-      await prefs.setString('email', emailJson);
-      await prefs.setString('phone', phoneJson);
-      await prefs.setString('profile_pic', profileJson);
-      setState(() {
-        user_name = prefs.getString('name')!;
-        user_email = prefs.getString('email')!;
-        user_phone = prefs.getString('phone')!;
-        user_profile_picture = prefs.getString('profile_pic')!;
-      });
-    } else {
-      // If the server did not return a 201 CREATED response,
-      // then throw an exception.
-      throw Exception('Failed to fetch data.');
-    }
-  }
-
-  List _loadedPhotos = [];
-
-  int currentIndex = 0;
-
-  getStringValuesSF() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    //Return String
-    String? stringValue = prefs.getString('accesToken');
-
-    setState(() {
-      user_id = prefs.getString('id')!;
-      user_type = prefs.getString('user_type')!;
-      access_Token = prefs.getString('accesToken')!;
-    });
-    const API_URL = 'http://192.168.0.117:8000/api/user/post-pharmacies';
-
-    final response = await http.get(
-      Uri.parse(API_URL),
-      headers: {
-        'Authorization': 'Bearer $access_Token',
-      },
-    );
-    final data = json.decode(response.body);
-    if (response.statusCode == 200) {
-      // If the server did return a 201 CREATED response,
-      // then parse the JSON.
-
-      print(response.body);
-      print("===========> done hh");
-      setState(() {
-        _loadedPhotos = data;
-        count = _loadedPhotos.length;
-      });
-    } else {
-      // If the server did not return a 201 CREATED response,
-      // then throw an exception.
-      print("failed test${response.body}");
-      setState(() {
-        _loadedPhotos = ["no posts yet"];
-        count = 0;
-      });
-    }
-  }
-
-  
 
   @override
   Widget build(BuildContext context) {
@@ -223,11 +49,60 @@ class _PatientHomeState extends State<PatientHome> {
           child: PharmaciesMap(),
         ));
   }
+    Future<void> getUserInfo() async {
+    final prefs = await SharedPreferences.getInstance();
+    final response = await http.get(
+      Uri.parse('http://192.168.0.117:8000/api/user/info?user_id=$user_id'),
+      headers: {
+        'Authorization': 'Bearer $access_Token',
+      },
+    );
 
+    if (response.statusCode == 201) {
+      // If the server did return a 201 CREATED response,
+      // then parse the JSON.
+      var responseJsondata = json.decode(response.body);
+
+      final String nameJson = (responseJsondata["user"][0]["name"]).toString();
+      final String emailJson =(responseJsondata["user"][0]["email"]).toString();
+      final String phoneJson =(responseJsondata["user"][0]["phone"]).toString();
+      final String profileJson =(responseJsondata["user"][0]["profile_pic"]).toString();
+
+      //set in shared prefs
+      await prefs.setString('name', nameJson);
+      await prefs.setString('email', emailJson);
+      await prefs.setString('phone', phoneJson);
+      await prefs.setString('profile_pic', profileJson);
+      setState(() {
+        user_name = prefs.getString('name')!;
+        user_email = prefs.getString('email')!;
+        user_phone = prefs.getString('phone')!;
+        user_profile_picture = prefs.getString('profile_pic')!;
+      });
+    } else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      throw Exception('Failed to fetch data.');
+    }
+  }
+
+
+
+  getSharedPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //Return String
+    //get shared prefs
+    setState(() {
+      user_id = prefs.getString('id')!;
+      user_type = prefs.getString('user_type')!;
+      access_Token = prefs.getString('accesToken')!;
+    });
+    
+  }
   @override
   initState() {
     super.initState();
-    getStringValuesSF();
+    getSharedPrefs();
     getUserInfo();
   }
 
